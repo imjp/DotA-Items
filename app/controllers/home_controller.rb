728bfec98825df6b1fa4dc1b924dc505d52011fa
Jean-Paul Bernadina <jpbernadina@gmail.com>
2012-02-01 01:11:13 -0430

    fixed logging in not working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
 	def index
     @heros = Hero.order('name ASC') 
-		@recent_builds = Build.recent
-		
-		
-		expires_in 5.minutes
-		fresh_when last_modified: @heros.maximum(:updated_at) 
+		@recent_builds = Build.recent 
 	end
 end
